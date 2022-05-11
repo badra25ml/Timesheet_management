7adf0314cf8f280a05f9943c7dabc4490cefdbd2
Aliou Sidi Maiga <badrasadja@gmail.com>
2022-05-11 14:32:18 +0000

    restructured the project

--- timesheetManagement/timesheetManagement/wsgi.py
+++ timesheetManagement/timesheetManagement/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for timesheet_management project.
+WSGI config for timesheetManagement project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'timesheet_management.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'timesheetManagement.settings')
 
 application = get_wsgi_application()
